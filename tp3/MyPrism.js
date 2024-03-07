58ed9dfd5bc59e39d1b02af54de7f7b4f72f4abf
Clarisse Carvalho <up202008444@fe.up.pt>
2024-03-07 02:12:02 +0000

    tp3-2

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -1,6 +1,6 @@
 import {CGFobject} from '../lib/CGF.js';
 /**
- * MyQuad
+ * MyPrism
  * @constructor
  * @param scene - Reference to MyScene object
  * @param slices - number of divisions around the Y axis
@@ -19,65 +19,63 @@ export class MyPrism extends CGFobject {
         this.indices = [];
         this.normals = [];
 
-        // height
-        for (var i = 0; i < this.stacks; i++) {
-            var h = i/this.stacks;
-            var hNext = (i+1)/this.stacks;
+        var h = 1/this.stacks;
+        for(var i = 0; i < this.stacks; i++){
             var ang = 0;
             var alphaAng = 2*Math.PI/this.slices;
+            
+            for(var j = 0; j < this.slices; j++){
+                var hCurrent = i*h
+                var hNext = (i+1)*h
 
-            for (var j = 0; j<this.slices; j++) {
-                
                 var sa=Math.sin(ang);
                 var saa=Math.sin(ang+alphaAng);
                 var ca=Math.cos(ang);
                 var caa=Math.cos(ang+alphaAng);
-
+    
                 this.vertices.push(ca,-sa,hNext);
-                this.vertices.push(ca,-sa,h);
-                this.vertices.push(caa,-saa,h);
+                this.vertices.push(ca,-sa,hCurrent);
+                this.vertices.push(caa,-saa,hCurrent);
                 this.vertices.push(caa,-saa,hNext);
     
-                // triangle normal computed by cross product of two edges
                 var normal= [
                     saa-sa,
-                    ca*saa-sa*caa,
-                    caa-ca
+                    caa-ca,
+                    0
                 ];
-
+    
                 // normalization
                 var nsize=Math.sqrt(
-                    normal[0]*normal[0]+ 
+                    normal[0]*normal[0]+
                     normal[1]*normal[1]+
                     normal[2]*normal[2]
                     );
                 normal[0]/=nsize;
                 normal[1]/=nsize;
                 normal[2]/=nsize;
-
-
+    
                 // 4 vertex
                 this.normals.push(...normal);
                 this.normals.push(...normal);
                 this.normals.push(...normal);
                 this.normals.push(...normal);
 
-                // Criar Prismas 
-                this.indices.push(4*j + 4*i*this.slices + 2, 4*j + 4*i*this.slices + 1,4*j + 4*i*this.slices);
-                this.indices.push(4*j + 4*i*this.slices + 3, 4*j + 4*i*this.slices + 2,4*j + 4*i*this.slices);
-
-                ang += alphaAng;
+                // create prism indices
+                var base = 4*j + 4*i*this.slices
+                this.indices.push(base + 2, base + 1, base);
+                this.indices.push(base + 3, base + 2, base);
+    
+                ang+=alphaAng;
             }
-
         }
+        		
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
 
+		this.initGLBuffers();
+	}
 
-
-        //The defined indices (and corresponding vertices)
-        //will be read in groups of three to draw triangles
-        this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();
-    }
     /**
      * Called when user interacts with GUI to change object's complexity.
      * @param {integer} complexity - changes number of slices
