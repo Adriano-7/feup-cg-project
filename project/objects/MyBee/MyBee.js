234b346d3b2e35edd884bd013ae666211846e442
Adriano Machado <up202105352@fe.up.pt>
2024-04-23 14:22:44 +0100

    Added a torax to the bee

--- project/objects/MyBee/MyBee.js
+++ project/objects/MyBee/MyBee.js
@@ -11,7 +11,8 @@ export class MyBee extends CGFobject {
     constructor(scene, texture) {
         super(scene);
         this.head = new MySphere(scene, 16, 16);
-        this.body = new MyEllipsoid(scene, 16, 16, [1.4, 1.2, 1.8]);
+        this.torax = new MyEllipsoid(scene, 16, 16, [1.4, 1.2, 1.3]);
+        this.abdomen = new MyEllipsoid(scene, 16, 16, [1.4, 1.2, 3.0]);
         this.texture = texture;
 
         this.material = new CGFappearance(scene);
@@ -24,15 +25,23 @@ export class MyBee extends CGFobject {
         this.scene.pushMatrix();
         this.material.apply();
 
-        // Display the body
-        this.scene.pushMatrix();
-        this.scene.translate(0, 0, 0.5);
-        this.body.display();
-        this.scene.popMatrix();
-
+        //Display the head
         this.scene.pushMatrix();
         this.scene.translate(0, 0, -1);
         this.head.display();
+        this.scene.popMatrix();
+
+        // Display the torax
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5);
+        this.torax.display();
+        this.scene.popMatrix();
+
+        // Display the abdomen
+        this.scene.pushMatrix();
+        this.scene.translate(0, -0.3, 1.5);
+        this.scene.rotate(Math.PI / 12, 1, 0, 0);
+        this.abdomen.display();
         this.scene.popMatrix();
 
         this.scene.popMatrix();
