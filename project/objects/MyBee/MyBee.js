5ddda02b40d663a14af6965914e172ee8fcb87a4
Adriano Machado <up202105352@fe.up.pt>
2024-04-23 16:22:42 +0100

    Added antennas to the bee

--- project/objects/MyBee/MyBee.js
+++ project/objects/MyBee/MyBee.js
@@ -1,6 +1,7 @@
 import { CGFobject, CGFappearance } from '../../../lib/CGF.js';
 import { MyEllipsoid } from "../../primitives/MyEllipsoid.js";
 import { MySphere } from "../../primitives/MySphere.js";
+import { MyCylinder } from "../../primitives/MyCylinder.js";
 
 /**
  * MyBee
@@ -11,6 +12,7 @@ export class MyBee extends CGFobject {
     constructor(scene, texture) {
         super(scene);
         this.head = new MySphere(scene, 16, 16);
+        this.antenna = new MyCylinder(scene, 16, 16);
         this.torax = new MyEllipsoid(scene, 16, 16, [1.4, 1.2, 1.3]);
         this.abdomen = new MyEllipsoid(scene, 16, 16, [1.4, 1.2, 3.0]);
         this.texture = texture;
@@ -29,6 +31,22 @@ export class MyBee extends CGFobject {
         this.scene.pushMatrix();
         this.scene.translate(0, 0, -1);
         this.head.display();
+        this.scene.popMatrix();
+
+        // Display the left antenna
+        this.scene.pushMatrix();
+        this.scene.translate(-0.8, 0.8, -2.3);
+        this.scene.rotate(Math.PI / 4, Math.PI / 4, 1, 0);
+        this.scene.scale(0.02, 0.02, 0.8);
+        this.antenna.display();
+        this.scene.popMatrix();
+
+        // Display the right antenna
+        this.scene.pushMatrix();
+        this.scene.translate(0.8, 0.8, -2.3);
+        this.scene.rotate(-Math.PI / 4, -Math.PI / 4, 1, 0);
+        this.scene.scale(0.02, 0.02, 0.8);
+        this.antenna.display();
         this.scene.popMatrix();
 
         // Display the torax
--- project/primitives/MyCylinder.js
+++ project/primitives/MyCylinder.js
@@ -1,4 +1,5 @@
-import { CGFobject } from '../lib/CGF.js';
+import { CGFobject } from '../../lib/CGF.js';
+
 /**
  * MyCylinder
  * @constructor
@@ -14,52 +15,106 @@ export class MyCylinder extends CGFobject {
         this.initBuffers();
     }
 
-    
     initBuffers() {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
-    
+        this.texCoords = []; // Added texture coordinates
+
         var h = 1 / this.stacks;
         var alphaAng = 2 * Math.PI / this.slices;
-    
+
         for (var i = 0; i < this.stacks; i++) {
             var ang = 0;
             var hCurrent = i * h;
             var hNext = (i + 1) * h;
-    
+
             for (var j = 0; j <= this.slices; j++) {
                 var sa = Math.sin(ang);
                 var ca = Math.cos(ang);
                 var sn = Math.sin(ang + alphaAng);
                 var cn = Math.cos(ang + alphaAng);
-    
+
                 this.vertices.push(ca, sa, hCurrent);
                 this.vertices.push(ca, sa, hNext);
-    
+
                 // 2 vertex
                 this.normals.push(ca, sa, 0);
                 this.normals.push(ca, sa, 0);
-    
+
+                // Added texture coordinates
+                this.texCoords.push(j / this.slices, hCurrent);
+                this.texCoords.push(j / this.slices, hNext);
+
                 if (j < this.slices) {
                     // create prism indices
                     var base = 2 * (i * (this.slices + 1) + j);
-                    this.indices.push(base, base + 2, base + 1); 
-                    this.indices.push(base + 2, base + 3, base + 1); 
+                    this.indices.push(base, base + 2, base + 1);
+                    this.indices.push(base + 2, base + 3, base + 1);
                 }
-    
+
                 ang += alphaAng;
             }
         }
-    
-		//The defined indices (and corresponding vertices)
-		//will be read in groups of three to draw triangles
+
+        for (let i = 0; i < this.slices; i++) {
+            this.vertices.push(0, 0, 0);
+            this.normals.push(0, 0, -1);
+            this.texCoords.push(0.5, 0.5);
+
+            let ang = i * alphaAng;
+            let sa = Math.sin(ang);
+            let ca = Math.cos(ang);
+
+            this.vertices.push(ca, sa, 0);
+            this.normals.push(0, 0, -1);
+            this.texCoords.push(ca / 2 + 0.5, -sa / 2 + 0.5);
+
+            ang = (i + 1) * alphaAng;
+            sa = Math.sin(ang);
+            ca = Math.cos(ang);
+
+            this.vertices.push(ca, sa, 0);
+            this.normals.push(0, 0, -1);
+            this.texCoords.push(ca / 2 + 0.5, -sa / 2 + 0.5);
+
+            let base = this.vertices.length / 3 - 3;
+            this.indices.push(base, base + 2, base + 1);
+        }
+
+        // Top
+        for (let i = 0; i < this.slices; i++) {
+            this.vertices.push(0, 0, 1);
+            this.normals.push(0, 0, 1);
+            this.texCoords.push(0.5, 0.5);
+
+            let ang = i * alphaAng;
+            let sa = Math.sin(ang);
+            let ca = Math.cos(ang);
+
+            this.vertices.push(ca, sa, 1);
+            this.normals.push(0, 0, 1);
+            this.texCoords.push(ca / 2 + 0.5, sa / 2 + 0.5);
+
+            ang = (i + 1) * alphaAng;
+            sa = Math.sin(ang);
+            ca = Math.cos(ang);
+
+            this.vertices.push(ca, sa, 1);
+            this.normals.push(0, 0, 1);
+            this.texCoords.push(ca / 2 + 0.5, sa / 2 + 0.5);
+
+            let base = this.vertices.length / 3 - 3;
+            this.indices.push(base, base + 1, base + 2);
+        }
+
+        //The defined indices (and corresponding vertices)
+        //will be read in groups of three to draw triangles
         this.primitiveType = this.scene.gl.TRIANGLES;
-    
+
         this.initGLBuffers();
     }
 
-    
 
     /**
      * Called when user interacts with GUI to change object's complexity.
