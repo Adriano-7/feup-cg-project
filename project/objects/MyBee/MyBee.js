753fcccf0b2ebb4cd31f4b4f73779a84d869283a
Adriano Machado <up202105352@fe.up.pt>
2024-04-25 11:06:41 +0100

    Added wings

--- project/objects/MyBee/MyBee.js
+++ project/objects/MyBee/MyBee.js
@@ -16,6 +16,7 @@ export class MyBee extends CGFobject {
         this.leg = new MyCylinder(scene, 16, 16);
         this.torax = new MyEllipsoid(scene, 16, 16, [1.3, 1.2, 1.7]);
         this.abdomen = new MyEllipsoid(scene, 16, 16, [1.2, 1.2, 1.3]);
+        this.wing = new MyEllipsoid(scene, 16, 16, [0.7, 0.1, 0.4]);
 
         this.headMaterial = new CGFappearance(scene);
         this.headMaterial.setEmission(1, 1, 1, 1);
@@ -31,25 +32,127 @@ export class MyBee extends CGFobject {
         this.abdomenMaterial.setEmission(1, 1, 1, 1);
         this.abdomenMaterial.setTexture(textureAbdomen);
         this.abdomenMaterial.setTextureWrap('REPEAT', 'REPEAT');
-
     }
 
     display() {
         this.scene.pushMatrix();
         this.headMaterial.apply();
 
+        // Display the 1st right leg
+        this.scene.pushMatrix();
+        this.scene.translate(0.8, -0.5, 0.3);
+        this.scene.rotate(3 * Math.PI / 6, 1, 1, 0);
+        this.scene.rotate(-Math.PI / 4, 0, 0, 1);
+        this.scene.scale(0.02, 0.02, 0.3);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(1.01, -1.1, 0.3);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.02, 0.02, 0.4);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        // Display the 1st  left leg
+        this.scene.pushMatrix();
+        this.scene.translate(-0.8, -0.5, 0.3);
+        this.scene.rotate(-3 * Math.PI / 6, -1, 1, 0);
+        this.scene.rotate(-Math.PI / 4, 0, 0, 1);
+        this.scene.scale(0.02, 0.02, 0.3);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-1.01, -1.1, 0.3);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.02, 0.02, 0.4);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        // Display the 2nd right leg
+        this.scene.pushMatrix();
+        this.scene.translate(0.8, -1, 1);
+        this.scene.rotate(3 * Math.PI / 6, 1, 1, 0);
+        this.scene.rotate(-Math.PI / 4, 0, 0, 1);
+        this.scene.scale(0.02, 0.02, 0.3);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(1.01, -1.7, 1);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.02, 0.02, 0.5);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        // Display the 2nd left leg
+        this.scene.pushMatrix();
+        this.scene.translate(-0.8, -1, 1);
+        this.scene.rotate(-3 * Math.PI / 6, -1, 1, 0);
+        this.scene.rotate(-Math.PI / 4, 0, 0, 1);
+        this.scene.scale(0.02, 0.02, 0.3);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-1.01, -1.7, 1);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.02, 0.02, 0.5);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        // Display the 3rd right leg
+        this.scene.pushMatrix();
+        this.scene.translate(0.8, -1.3, 1.7);
+        this.scene.rotate(3 * Math.PI / 6, 1, 1, 0);
+        this.scene.rotate(-Math.PI / 4, 0, 0, 1);
+        this.scene.scale(0.02, 0.02, 0.3);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(1.01, -2, 1.7);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.02, 0.02, 0.5);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        // Display the 3rd left leg
+        this.scene.pushMatrix();
+        this.scene.translate(-0.8, -1.3, 1.7);
+        this.scene.rotate(-3 * Math.PI / 6, -1, 1, 0);
+        this.scene.rotate(-Math.PI / 4, 0, 0, 1);
+        this.scene.scale(0.02, 0.02, 0.3);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-1.01, -2, 1.7);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.02, 0.02, 0.5);
+        this.leg.display();
+        this.scene.popMatrix();
+
+        // Display the right wing
+        this.scene.pushMatrix();
+        this.scene.translate(1.8, -0.2, 1.2);
+        this.scene.rotate(Math.PI / 12, 1, 0, 0);
+        this.wing.display();
+        this.scene.popMatrix();
+
+        // Display the left wing
+        this.scene.pushMatrix();
+        this.scene.translate(-1.8, -0.2, 1.2);
+        this.scene.rotate(Math.PI / 12, 1, 0, 0);
+        this.wing.display();
+        this.scene.popMatrix();
+
+
         //Display the head
         this.scene.pushMatrix();
         this.scene.translate(0, 0, -1);
         this.head.display();
-        this.scene.popMatrix();
-
-        // Display the right leg
-        this.scene.pushMatrix();
-        this.scene.rotate(Math.PI / 3, 1, 0, 0);
-        this.scene.translate(-0.5, -1.5, 0.5);
-        this.scene.scale(0.02, 0.02, 0.8);
-        this.leg.display();
         this.scene.popMatrix();
 
         // Display the abdomen
