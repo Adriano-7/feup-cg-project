3a2862cf203de01b3fd9759a8bfb60f8f692769e
Adriano Machado <up202105352@fe.up.pt>
2024-05-18 12:16:34 +0100

    Added back indexes to the cube

--- project/oldClasses/MyCube.js
+++ project/oldClasses/MyCube.js
@@ -15,33 +15,33 @@ export class MyCube extends CGFobject {
         this.vertices = [
             // Front face
             -this.size / 2, -this.size / 2, this.size / 2, // 0
-            this.size / 2, -this.size / 2, this.size / 2,  // 1
-            this.size / 2, this.size / 2, this.size / 2,   // 2
-            -this.size / 2, this.size / 2, this.size / 2,  // 3
+            this.size / 2, -this.size / 2, this.size / 2, // 1
+            this.size / 2, this.size / 2, this.size / 2, // 2
+            -this.size / 2, this.size / 2, this.size / 2, // 3
             // Back face
             -this.size / 2, -this.size / 2, -this.size / 2, // 4
-            this.size / 2, -this.size / 2, -this.size / 2,  // 5
-            this.size / 2, this.size / 2, -this.size / 2,   // 6
-            -this.size / 2, this.size / 2, -this.size / 2,  // 7
+            this.size / 2, -this.size / 2, -this.size / 2, // 5
+            this.size / 2, this.size / 2, -this.size / 2, // 6
+            -this.size / 2, this.size / 2, -this.size / 2, // 7
             // Top face
-            -this.size / 2, this.size / 2, this.size / 2,   // 8
-            this.size / 2, this.size / 2, this.size / 2,    // 9
-            this.size / 2, this.size / 2, -this.size / 2,   // 10
-            -this.size / 2, this.size / 2, -this.size / 2,  // 11
+            -this.size / 2, this.size / 2, this.size / 2, // 8
+            this.size / 2, this.size / 2, this.size / 2, // 9
+            this.size / 2, this.size / 2, -this.size / 2, // 10
+            -this.size / 2, this.size / 2, -this.size / 2, // 11
             // Bottom face
-            -this.size / 2, -this.size / 2, this.size / 2,  // 12
-            this.size / 2, -this.size / 2, this.size / 2,   // 13
-            this.size / 2, -this.size / 2, -this.size / 2,  // 14
+            -this.size / 2, -this.size / 2, this.size / 2, // 12
+            this.size / 2, -this.size / 2, this.size / 2, // 13
+            this.size / 2, -this.size / 2, -this.size / 2, // 14
             -this.size / 2, -this.size / 2, -this.size / 2, // 15
             // Right face
-            this.size / 2, -this.size / 2, this.size / 2,   // 16
-            this.size / 2, this.size / 2, this.size / 2,    // 17
-            this.size / 2, this.size / 2, -this.size / 2,   // 18
-            this.size / 2, -this.size / 2, -this.size / 2,  // 19
+            this.size / 2, -this.size / 2, this.size / 2, // 16
+            this.size / 2, this.size / 2, this.size / 2, // 17
+            this.size / 2, this.size / 2, -this.size / 2, // 18
+            this.size / 2, -this.size / 2, -this.size / 2, // 19
             // Left face
-            -this.size / 2, -this.size / 2, this.size / 2,  // 20
-            -this.size / 2, this.size / 2, this.size / 2,   // 21
-            -this.size / 2, this.size / 2, -this.size / 2,  // 22
+            -this.size / 2, -this.size / 2, this.size / 2, // 20
+            -this.size / 2, this.size / 2, this.size / 2, // 21
+            -this.size / 2, this.size / 2, -this.size / 2, // 22
             -this.size / 2, -this.size / 2, -this.size / 2, // 23
         ];
 
@@ -64,6 +64,26 @@ export class MyCube extends CGFobject {
             // Left face
             20, 22, 21,
             20, 23, 22,
+
+            // Adding the reverse (clockwise) order
+            // Front face
+            2, 1, 0,
+            3, 2, 0,
+            // Back face
+            5, 6, 4,
+            6, 7, 4,
+            // Top face
+            10, 9, 8,
+            11, 10, 8,
+            // Bottom face
+            13, 14, 12,
+            14, 15, 12,
+            // Right face
+            17, 18, 16,
+            18, 19, 16,
+            // Left face
+            21, 22, 20,
+            22, 23, 20,
         ];
 
         this.normals = [
