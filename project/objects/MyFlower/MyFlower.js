880041c57c75f9e05a048ccc7979e649fe132e60
Adriano Machado <up202105352@fe.up.pt>
2024-05-20 09:09:27 +0100

    Added random flower color

--- project/objects/MyFlower/MyFlower.js
+++ project/objects/MyFlower/MyFlower.js
@@ -16,36 +16,49 @@ export class MyFlower extends CGFobject {
         this.y = 0;
         this.z = 0;
 
+        this.flowerType = Math.floor(Math.random() * 2) + 1;
+
         this.externalRadius = externalRadius;
         this.numberOfPetals = numberOfPetals;
         this.receptacleRadius = receptacleRadius;
         this.stemRadius = stemRadius;
         this.stemHeight = stemHeight;
 
-        this.texturePetals = new CGFtexture(scene, "textures/flower1Petals.jpg");
-        this.petalMaterial = new CGFappearance(scene);
-        this.petalMaterial.setEmission(1, 1, 1, 1);
-        this.petalMaterial.setTexture(this.texturePetals);
-        this.petalMaterial.setTextureWrap('REPEAT', 'REPEAT');
+        this.texturePetals1 = new CGFtexture(scene, "textures/flower1Petals.jpg");
+        this.petal1Material = new CGFappearance(scene);
+        this.petal1Material.setEmission(1, 1, 1, 1);
+        this.petal1Material.setTexture(this.texturePetals1);
+        this.petal1Material.setTextureWrap('REPEAT', 'REPEAT');
 
-        this.textureReceptacle = new CGFtexture(scene, "textures/flowerReceptacle1.jpg");
-        this.receptacleMaterial = new CGFappearance(scene);
-        this.receptacleMaterial.setEmission(1, 1, 1, 1);
-        this.receptacleMaterial.setTexture(this.textureReceptacle);
-        this.receptacleMaterial.setTextureWrap('REPEAT', 'REPEAT');
+        this.textureReceptacle1 = new CGFtexture(scene, "textures/flowerReceptacle1.jpg");
+        this.receptacleMaterial1 = new CGFappearance(scene);
+        this.receptacleMaterial1.setEmission(1, 1, 1, 1);
+        this.receptacleMaterial1.setTexture(this.textureReceptacle1);
+        this.receptacleMaterial1.setTextureWrap('REPEAT', 'REPEAT');
 
-        this.textureStem = new CGFtexture(scene, "textures/flowerStem1.jpg");
-        this.stemMaterial = new CGFappearance(scene);
-        this.stemMaterial.setEmission(1, 1, 1, 1);
-        this.stemMaterial.setTexture(this.textureStem);
-        this.stemMaterial.setTextureWrap('REPEAT', 'REPEAT');
+        this.textureStem1 = new CGFtexture(scene, "textures/flowerStem1.jpg");
+        this.stemMaterial1 = new CGFappearance(scene);
+        this.stemMaterial1.setEmission(1, 1, 1, 1);
+        this.stemMaterial1.setTexture(this.textureStem1);
+        this.stemMaterial1.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.texturePetals2 = new CGFtexture(scene, "textures/flower2Petals.jpg");
+        this.petal2Material = new CGFappearance(scene);
+        this.petal2Material.setEmission(1, 1, 1, 1);
+        this.petal2Material.setTexture(this.texturePetals2);
+        this.petal2Material.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.textureReceptacle2 = new CGFtexture(scene, "textures/flowerReceptacle2.jpg");
+        this.receptacleMaterial2 = new CGFappearance(scene);
+        this.receptacleMaterial2.setEmission(1, 1, 1, 1);
+        this.receptacleMaterial2.setTexture(this.textureReceptacle2);
+        this.receptacleMaterial2.setTextureWrap('REPEAT', 'REPEAT');
 
         this.textureLeaf = new CGFtexture(scene, "textures/leaf.jpg");
         this.leafMaterial = new CGFappearance(scene);
         this.leafMaterial.setEmission(1, 1, 1, 1);
         this.leafMaterial.setTexture(this.textureLeaf);
         this.leafMaterial.setTextureWrap('REPEAT', 'REPEAT');
-
 
         let lPetals = externalRadius - receptacleRadius
         let nLayers = 2;
@@ -62,19 +75,19 @@ export class MyFlower extends CGFobject {
 
         // Display petals
         this.scene.pushMatrix();
-        this.petalMaterial.apply();
+        this.flowerType === 1 ? this.petal1Material.apply() : this.petal2Material.apply();
         this.petals.display();
         this.scene.popMatrix();
 
         // Display receptacle
         this.scene.pushMatrix();
-        this.receptacleMaterial.apply();
+        this.flowerType === 1 ? this.receptacleMaterial1.apply() : this.receptacleMaterial2.apply();
         this.receptacle.display();
         this.scene.popMatrix();
 
         // Display stem
         this.scene.pushMatrix();
-        this.stemMaterial.apply();
+        this.stemMaterial1.apply();
         this.stem.display();
         this.scene.popMatrix();
 
